2a0db07701df0761ffdfee2c6f9f38fa0766b295
Nicole Lopes <nicole.cls25@gmail.com>
2024-07-19 11:17:42 -0300

    Atualização do router com:  condições de navegação e autenticação de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,27 @@
+
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import UserManagement from '../views/UserManagement.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/users', component: UserManagement, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('user');
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
+
